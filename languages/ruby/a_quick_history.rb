2b7a4fb0a0a28da4fd03214a7c75a30791fce87f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2021-04-17 19:25:22 -0300

    [ruby] a quick history

---
+++ languages/ruby/a_quick_history.rb
@@ -0,0 +1,10 @@
+properties = ['object oriented' , 'duck typed' , 'productive' , 'fun' ]
+#  => ["object oriented", "duck typed", "productive", "fun"] 
+
+properties.each {|property| puts "Ruby is #{property}." }
+# Ruby is object oriented.
+# Ruby is duck typed.
+# Ruby is productive.
+# Ruby is fun.
+#  => ["object oriented", "duck typed", "productive", "fun"] 
+
